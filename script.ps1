160cfe2187a3b15da261b51e7a2e2f1c3bc09043
mostafahery <omar.oda22@yahoo.com>
2022-08-19 15:54:18 +0100

    Updated README with new features

--- script.ps1
+++ script.ps1
@@ -4,3 +4,4 @@ Temporary commit content for 2022-07-17T15:54:18
 Temporary commit content for 2022-07-18T15:54:18
 Temporary commit content for 2022-07-21T15:54:18
 Temporary commit content for 2022-07-31T15:54:18
+Temporary commit content for 2022-08-19T15:54:18
