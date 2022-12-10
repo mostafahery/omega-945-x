75a1388565d7b9acb66624d0e5b303a212d4d7ef
mostafahery <omar.oda22@yahoo.com>
2022-12-10 15:54:18 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -12,3 +12,4 @@ Temporary commit content for 2022-09-24T15:54:18
 Temporary commit content for 2022-10-03T15:54:18
 Temporary commit content for 2022-10-19T15:54:18
 Temporary commit content for 2022-11-29T15:54:18
+Temporary commit content for 2022-12-10T15:54:18
