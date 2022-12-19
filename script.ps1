c7b6fd7646cd686ebf7900ca26ad65fff5d9c2f3
mostafahery <omar.oda22@yahoo.com>
2022-12-19 15:54:18 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -15,3 +15,4 @@ Temporary commit content for 2022-11-29T15:54:18
 Temporary commit content for 2022-12-10T15:54:18
 Temporary commit content for 2022-12-15T15:54:18
 Temporary commit content for 2022-12-18T15:54:18
+Temporary commit content for 2022-12-19T15:54:18
