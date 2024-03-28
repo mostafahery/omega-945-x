d21a456f7e0cea21318edb5b288bb5ad8b29377c
mostafahery <omar.oda22@yahoo.com>
2024-03-28 15:54:18 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -46,3 +46,4 @@ Temporary commit content for 2024-03-08T15:54:18
 Temporary commit content for 2024-03-10T15:54:18
 Temporary commit content for 2024-03-11T15:54:18
 Temporary commit content for 2024-03-13T15:54:18
+Temporary commit content for 2024-03-28T15:54:18
