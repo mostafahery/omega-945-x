bda533936297e91c92908a9d76b71072288e4f61
mostafahery <omar.oda22@yahoo.com>
2025-01-02 15:54:18 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -77,3 +77,4 @@ Temporary commit content for 2024-12-20T15:54:18
 Temporary commit content for 2024-12-24T15:54:18
 Temporary commit content for 2024-12-27T15:54:18
 Temporary commit content for 2024-12-29T15:54:18
+Temporary commit content for 2025-01-02T15:54:18
